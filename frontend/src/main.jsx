f6409ba3a1bc1acfe7eb0da521260ba05830544f
Aman Verma <amanverma@gmail.com>
2026-03-28 14:57:37 +0530

    first commit videocall  me simple peer fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-import process from "process"
-import { Buffer } from "buffer"
+import { Buffer } from "buffer";
+import process from "process";
 
-window.global = window;
-window.process = process;
 window.Buffer = Buffer;
+window.process = process;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
